4298da6f3a01b34590fc1a964f1116b64b228e92
Srishti Kumari <kumarsinghnarendar018@gmail.com>
2025-04-16 13:31:36 +0530

    new commit

--- TT/jagged_array.py
+++ TT/jagged_array.py
@@ -3,7 +3,7 @@ jagged_array = [
     [1, 2, 3],          # Row 1 with 3 elements
     [4, 5],             # Row 2 with 2 elements
     [6, 7, 8, 9],       # Row 3 with 4 elements
-    [10]                # Row 4 with 1 el
+    [10]              
 ]
 
 for row in jagged_array:
